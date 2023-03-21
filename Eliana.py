8b8611586a089741fbb24a0a6362ab64a4a6032d
Eliana <Elianalmeida@gmail.com>
2023-03-21 09:28:42 -0300

    Mutilicacao

--- Eliana.py
+++ Eliana.py
@@ -1,4 +1,7 @@
-def soma():
-    resultado = n1 + n2
-    return resultado
+def soma(n1,n2):
+    resultado1 = n1 + n2
+    return resultado1
 
+def multiplicaao(n1,n2):
+    resultado2 = n1 * n2
+    return resultado2
